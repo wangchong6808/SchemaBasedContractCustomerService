011b897e6301039ad6aee262fc4c055b03766a77
WangChong <wangchong6808@163.com>
2019-06-04 11:56:15 +0800

    removed -u root from build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #! /usr/bin/env bash
 
 echo `id`
-docker run --rm   -v $(pwd):/opt/app -w /opt/app -u root gradle:4.8-jdk8-alpine gradle clean bootRepackage
+docker run --rm   -v $(pwd):/opt/app -w /opt/app gradle:4.8-jdk8-alpine gradle clean bootRepackage
